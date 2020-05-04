1496a615d4071ba5d53f4180e7d8e0cfbfb8658d
lausek <root@lausek.eu>
2020-05-04 23:44:20 +0200

    add tests for objects

--- test/test_objects.py
+++ test/test_objects.py
@@ -38,3 +38,38 @@ und zwar return
         ret = internals.interpreter.call('return')
         self.assertEqual(1, float(ret['b']))
         self.assertEqual(2, float(ret['sub']['a']))
+
+    def test_object_passing(self, internals):
+        src = """
+und zwar select_one mit m
+    m.b und fertig
+
+und zwar give
+    obj1 ist menge
+    obj1.a ist 1
+    obj1.b ist 2
+    obj1.c ist 3
+
+    (select_one obj1) und fertig
+        """
+        program = internals.compiler.compile(src)
+        internals.interpreter.load(program)
+
+        ret = internals.interpreter.call('give')
+        self.assertEqual(2, float(ret))
+
+    def test_object_number_key(self, internals):
+        src = """
+und zwar give
+    obj1 ist menge
+    obj1.1 ist "first"
+    obj1.2 ist "second"
+    obj1.3 ist "third"
+
+    obj1 und fertig
+        """
+        program = internals.compiler.compile(src)
+        internals.interpreter.load(program)
+
+        ret = internals.interpreter.call('give')
+        self.assertEqual('second', str(ret['2']))
