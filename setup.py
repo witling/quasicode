2b5293883936c3ce823d0b2c3fc5164c34d5c124
lausek <root@lausek.eu>
2020-05-07 22:35:59 +0200

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
-from setuptools import find_package, setup
+from setuptools import find_packages, setup
 
 setup(
     name='quasicode',
     version='0.0.1',
+    author='witling',
     description='the best programming language around.',
+    url='https://github.com/witling/quasicode',
     packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
     install_requires=[
         'dill'
     ],
+    python_requires='>=3.5',
     entry_points={'console_scripts': ['qc=qc.__main__:main']}
 )
