83bd472fc78b5d1d47edee6a882ec2e6274b1775
lausek <root@lausek.eu>
2020-05-19 18:59:25 +0200

    remove useless code

--- qclib/program.py
+++ qclib/program.py
@@ -3,12 +3,7 @@ from .library import *
 class Program(Library):
     def __init__(self):
         super().__init__()
-        # TODO: is flow needed?
-        self._flow = []
         self._entry_point = None
-
-    def __iter__(self):
-        return iter(self._flow)
 
     def entry_point(self):
         return self._entry_point
